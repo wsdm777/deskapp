f8b9f8eddc55953521b6f73e3acc645bc6546427
wsdm <kuznetsov06denis@yandex.ru>
2024-11-22 22:02:36 +0300

    auth form

--- main.py
+++ main.py
@@ -1,56 +1,34 @@
 import sys
+from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import (
-    QApplication,
-    QWidget,
-    QVBoxLayout,
-    QLineEdit,
-    QPushButton,
-    QLabel,
-)
-
+from authwindow import Ui_Form as authForm
 from repository import (
     APIClient,
 )
 
 
-class LoginForm(QWidget):
+class LoginForm(QWidget, authForm):
     def __init__(self):
         super().__init__()
-
+        self.setupUi(self)
         self.api = APIClient("http://127.0.0.1:8000")
-
-        self.setWindowTitle("Авторизация")
-        self.setGeometry(100, 100, 300, 150)
-
-        self.username_label = QLabel("Почта:")
-        self.password_label = QLabel("Пароль:")
-
-        self.username_input = QLineEdit()
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-
-        self.login_button = QPushButton("Войти")
-        self.login_button.clicked.connect(self.on_login_clicked)
-
-        form_layout = QVBoxLayout()
-        form_layout.addWidget(self.username_label)
-        form_layout.addWidget(self.username_input)
-        form_layout.addWidget(self.password_label)
-        form_layout.addWidget(self.password_input)
-        form_layout.addWidget(self.login_button)
-
-        self.setLayout(form_layout)
+        self.pushButton.clicked.connect(self.on_login_clicked)
 
     def on_login_clicked(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
         self.login(username, password)
 
     def login(self, username, password):
-        self.api.login(username, password)
-        res = self.api.me()
-        print(res)
+        try:
+            self.api.login(username, password)
+            res = self.api.me()
+            print(res)
+        except ValueError:
+            self.show_error()
+
+    def show_error(self):
+        self.errorLabel.setText("Логин или пароль неверный")
 
     def closeEvent(self, event):
         self.api.close_session()
