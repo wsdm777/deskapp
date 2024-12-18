b7d01d613d16721339bc9d513920946452bc36e5
wsdm <kuznetsov06denis@yandex.ru>
2024-12-18 21:58:24 +0300

    add update head section func

--- src/repository/crud/section/section.py
+++ src/repository/crud/section/section.py
@@ -1,5 +1,4 @@
-from tkinter import SE
-from sqlalchemy import delete, select
+from sqlalchemy import delete, select, update
 from src.repository.crud.section.schemas import SectionCreate
 from src.repository.models import Position, Section, User
 from src.repository.database import get_session
@@ -20,9 +19,29 @@ async def add_section(data: SectionCreate):
         return 1
 
 
-async def delete_section(name: str):
+async def update_section_head(section_name: str, new_head_email):
     async with get_session() as session:
-        stmt = delete(Section).where(Section.name == name)
+        stmt = (
+            update(Section)
+            .where(Section.name == section_name)
+            .values(Section.head_email == new_head_email)
+        )
+
+        result = await session.execute(stmt)
+
+        if result.rowcount == 0:
+            logger.error(f"Section {section_name} not found")
+            return 0
+
+        logger.info(f"Change head section: new_head = {new_head_email}")
+        await session.commit()
+
+        return 1
+
+
+async def delete_section(section_name: str):
+    async with get_session() as session:
+        stmt = delete(Section).where(Section.name == section_name)
 
         result = await session.execute(stmt)
 
@@ -31,10 +50,10 @@ async def delete_section(name: str):
 
         # Запрос не затронул ни одну строку функция возвращает 0 и логгер записывает ошибку
         if result.rowcount == 0:
-            logger.error(f"Section {name} not found")
+            logger.error(f"Section {section_name} not found")
             return 0
 
-        logger.info(f"Deleted section {name}")
+        logger.info(f"Deleted section {section_name}")
         return 1
 
 
@@ -46,5 +65,17 @@ async def get_section_by_name(section_name: str):
             .filter(Section.name == section_name)
         )
         result = await session.execute(query)
-        result = result.unique().all()
+        result = result.unique().one_or_none()
+        if result is None:
+            logger.error(f"Section {section_name} not found")
+            return 0
+
         return result
+
+
+async def get_all_sections():
+    async with get_session() as session:
+        query = select(Section)
+        result = await session.execute(query)
+        sections = result.all()
+        return sections
